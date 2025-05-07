0fdc099b5187e9742f509c5a65ec3697950f7969
WR <ryo.watanabe@rakus-partners.co.jp>
2025-05-07 17:07:54 +0900

    iam_roleの設定

--- terraform/iam_roles.tf
+++ terraform/iam_roles.tf
@@ -1,18 +1,18 @@
-resource "aws_iam_role" "power-forecasting-role" {
+resource "aws_iam_role" "power_forecasting_role" {
   assume_role_policy = jsonencode(
     {
+      Version = "2012-10-17"
       Statement = [
         {
-          Action = "sts:AssumeRole"
-          Effect = "Allow"
           Principal = {
             Service = [
               "sagemaker.amazonaws.com",
             ]
           }
+          Action = "sts:AssumeRole"
+          Effect = "Allow"
         },
       ]
-      Version = "2012-10-17"
     }
   )
   force_detach_policies = false
@@ -25,7 +25,33 @@ resource "aws_iam_role" "power-forecasting-role" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "attach_s3_read_policy" {
-  role       = aws_iam_role.power-forecasting-role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+resource "aws_iam_policy" "power_forecasting_policy" {
+  name = "power-forecasting-${terraform.workspace}"
+  path = "/service-role/"
+  policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Effect = "Allow"
+          # ファイルの取得、リスト、モデルの保存を想定
+          Action = [
+            "s3:GetObject",
+            "s3:ListBucket",
+            "s3:PutObject"
+          ]
+          # バケットとその中のオブジェクトに対する操作
+          Resource = [
+            "arn:aws:s3:::power-forecasting-mlops-${terraform.workspace}",
+            "arn:aws:s3:::power-forecasting-mlops-${terraform.workspace}/*"
+          ]
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "power_forecasting_policy_attachment" {
+  role       = aws_iam_role.power_forecasting_role.name
+  policy_arn = aws_iam_policy.power_forecasting_policy.arn
 }
